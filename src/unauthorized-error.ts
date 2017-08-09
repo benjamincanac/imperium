377d6a6ba968cbcf4f270edf7426c1384d8288a2
Benjamin Canac <canacb1@gmail.com>
2017-08-09 21:11:51 +0200

    Handle context error

--- src/unauthorized-error.ts
+++ src/unauthorized-error.ts
@@ -1,10 +1,12 @@
 class UnauthorizedError extends Error {
 	public statusCode
+	public context
 
-	constructor(statusCode: number, m: string) {
+	constructor(statusCode: number, m: string, context?: object) {
 		super(m)
 
 		this.statusCode = statusCode || 500
+		this.context = context || {}
 
 		Object.setPrototypeOf(this, UnauthorizedError.prototype)
 	}
